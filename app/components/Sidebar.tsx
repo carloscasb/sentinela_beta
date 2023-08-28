9832117148c66d17f7f77295db6a45f40825c10d
Carlos Barbosa <carlosbarbosa.casb@gmail.comm>
2023-08-28 17:31:06 -0300

    corregindo do projeto sem icons

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-"use cliente"
+
 import Image from "next/image"
 import Link from "next/link"
 //import {IconeCasa, IconeEventos, IconeFaccoes,  IconeOrganizacoes,  IconePessoas, IconeSistema, IconeUsuarios} from '../icons/page'
@@ -8,7 +8,8 @@ import { FaBeer } from "react-icons/fa";
 
 import { BiCog } from 'react-icons/bi'
 
-import { HiOutlineUsers } from 'react-icons/hi'
+import { HiHome, HiOutlineFire, HiOutlineFlag, HiOutlineUsers,  HiOutlineCog, HiOutlineUserGroup, HiOutlineScale, HiOutlineHome, HiOutlineKey } from 'react-icons/hi'
+
 
 export default function Sidebar() {
     return ( 
@@ -27,31 +28,62 @@ export default function Sidebar() {
                  <div className="flex-1">
                     <ul  className="pt-2 pb-4 space-y-1 text-sm" >
                        
+                    <li className="rounded-sm">
+                            <Link href='/' className="flex items-center p-3 space-x-4 rounded-md">
+                            <HiOutlineHome size={30} />
+                                <span>Inícios</span>
+                            </Link>
+                        </li>
 
                         <li className="rounded-sm">
-                            <Link href='/site/sistema' className="flex items-center p-5 space-x-4 rounded-sm">
-                            <FaBeer />
-                           
+                            <Link href='/site/evento' className="flex items-center p-3 space-x-4 rounded-md">
+                            <HiOutlineFire size={30}/>
+                                <span>Eventos</span>
+                            </Link>
+                        </li>
+
+                        <li className="rounded-sm">
+                            <Link href='/site/pessoa' className="flex items-center p-3 space-x-4 rounded-md">
+                                <HiOutlineUsers size={30}/>
+                                <span>Pessoas</span>
+                            </Link>
+                        </li>
+
+                        <li className="rounded-sm">
+                            <Link href='/site/faccao' className="flex items-center p-3 space-x-4 rounded-md">
+                                <HiOutlineFlag size={30}/>
+                                <span>Facções</span>
+                            </Link>
+                        </li>
+
+
+                        <li className="rounded-sm">
+                            <Link href='/site/organizacao' className="flex items-center p-3 space-x-4 rounded-md">
+                                <HiOutlineScale size={30}/>
+                                <span>Organizações</span>
+                            </Link>
+                        </li>
+
+                        <li className="rounded-sm">
+                            <Link href='/site/usuario' className="flex items-center p-3 space-x-4 rounded-md">
+                                <HiOutlineUserGroup size={30}/>
+                                <span>Usuários</span>
+                            </Link>
+                        </li>
+
+                        <li className="rounded-sm">
+                            <Link href='/site/sistema' className="flex items-center p-3 space-x-4 rounded-md">
+                                <HiOutlineCog size={30}/>
                                 <span>Sistema</span>
                             </Link>
                         </li>
                         
-                        <li className=" rounded-xl">
-                            <Link href='/site/faccao' className="flex items-center p-5 space-x-4  rounded-none ">
-                            
-                           < BiCog/>
-                                <span>Outros</span>
+                        <li className="rounded-sm">
+                            <Link href='/site/suporte' className="flex items-center p-3 space-x-4 rounded-md">
+                                <HiOutlineKey size={30}/>
+                                <span>Suporte</span>
                             </Link>
                         </li>
-
-                        <li className=" rounded-xl">
-                            <Link href='/site/faccao' className="flex items-center p-5 space-x-4  rounded-none ">
-                            
-                           < HiOutlineUsers/>
-                                <span>Outros</span>
-                            </Link>
-                        </li>
-                        
                         
                     </ul>
                     </div>
